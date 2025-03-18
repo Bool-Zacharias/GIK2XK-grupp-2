02f3d76514aa5fba1107396621e0f3a6353528b7
patrikpeterson69 <h23patpe@du.se>
2025-03-18 10:15:59 +0000

    skdjbgvsdkj

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const db = require('../models');
 const validate = require('validate.js');
-const postService = require('../services/postService');
 
 const constraints = {
   email: {
@@ -23,63 +22,62 @@ const constraints = {
       tooLong: '^Användarnamnet får inte vara längre än %{count} tecken långt.'
     }
   },
-  imageUrl: {
-    url: {
-      message: '^Sökvägen är felaktig.'
+  password: {
+    length: {
+      minimum: 6,
+      message: '^Lösenordet måste vara minst %{count} tecken långt.'
     }
   }
 };
 
-router.get('/:id/posts', (req, res) => {
-  const id = req.params.id;
-
-  postService.getByAuthor(id).then((result) => {
-    res.status(result.status).json(result.data);
-  });
-});
-
+// Hämta alla användare (administratörsfunktion)
 router.get('/', (req, res) => {
-  db.user.findAll().then((result) => {
-    res.send(result);
+  db.user.findAll({ attributes: { exclude: ['password'] } }).then((users) => {
+    res.json(users);
   });
 });
 
+// Hämta en specifik användare via ID
+router.get('/:id', (req, res) => {
+  db.user.findByPk(req.params.id, { attributes: { exclude: ['password'] } })
+    .then((user) => {
+      if (user) {
+        res.json(user);
+      } else {
+        res.status(404).json({ error: 'Användaren hittades inte' });
+      }
+    });
+});
+
+// Skapa en ny användare (Registrering)
 router.post('/', (req, res) => {
   const user = req.body;
   const invalidData = validate(user, constraints);
   if (invalidData) {
     res.status(400).json(invalidData);
   } else {
-    db.user.create(user).then((result) => {
-      res.send(result);
+    db.user.create(user).then((newUser) => {
+      res.status(201).json({ id: newUser.id, username: newUser.username, email: newUser.email });
     });
   }
 });
 
+// Uppdatera användare
 router.put('/', (req, res) => {
   const user = req.body;
   const invalidData = validate(user, constraints);
-  const id = user.id;
-  if (invalidData || !id) {
+  if (invalidData || !user.id) {
     res.status(400).json(invalidData || 'Id är obligatoriskt.');
   } else {
-    db.user
-      .update(user, {
-        where: { id: user.id }
-      })
-      .then((result) => {
-        res.send('Inlägget har uppdaterats.');
-      });
+    db.user.update(user, { where: { id: user.id } })
+      .then(() => res.send('Användaren har uppdaterats.'));
   }
 });
+
+// Ta bort användare
 router.delete('/', (req, res) => {
-  db.user
-    .destroy({
-      where: { id: req.body.id }
-    })
-    .then(() => {
-      res.json(`Inlägget raderades`);
-    });
+  db.user.destroy({ where: { id: req.body.id } })
+    .then(() => res.json('Användaren har raderats.'));
 });
 
 module.exports = router;
