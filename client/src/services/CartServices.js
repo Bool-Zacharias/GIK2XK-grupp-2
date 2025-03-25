e9d6d74572df6a84be7c67b30e318fa751f2c895
Bool-Zacharias <h23zacan@du.se>
2025-03-25 10:29:23 +0100

    Add to cart

--- client/src/services/CartServices.js
+++ client/src/services/CartServices.js
@@ -1,6 +1,6 @@
 import React, { createContext, useState, useContext } from "react";
 
-const CartContext = createContext();
+/* const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
@@ -25,4 +25,21 @@ export const CartProvider = ({ children }) => {
   );
 };
 
-export const useCart = () => useContext(CartContext);
+export const useCart = () => useServices(CartServices) */;
+
+const API_BASE_URL = "http://localhost:3000/product";
+import React, { createContext, useState, useContext } from "react";
+
+export const addToCart = async (product, id,user_id ) => {
+    try {
+      const response = await axios.post(`${API_BASE_URL}/${id}/addToCart`, product, user_id, {
+        user_id: user_id,
+        amount: 1, 
+      });
+  
+      return response.data;
+    } catch (error) {
+      console.error("Kunde inte lägga till i varukorgen:", error);
+      throw error;
+    }
+  };
